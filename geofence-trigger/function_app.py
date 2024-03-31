52e3db695b4215d39a4d66c60f6ee7f771020364
Mai Thi Yen Nhi <135790309+nima321123@users.noreply.github.com>
2024-03-31 23:45:31 +0700

    Update function_app.py

--- geofence-trigger/function_app.py
+++ geofence-trigger/function_app.py
@@ -7,11 +7,29 @@ import requests
 
 app = func.FunctionApp()
 
+def send_email(subject, message):
+    recipient_email = "10422015@student.vgu.edu.vn"
+    logging.info('send_email function called')  
+    url = f"https://api.mailgun.net/v3/{os.environ['MAILGUN_DOMAIN']}/messages"
+
+    data = {
+        "from": os.environ['MAILGUN_FROM_EMAIL'],
+        "to": recipient_email,
+        "subject": subject,
+        "text": message
+    }
+
+    response = requests.post(url, auth=("api", os.environ['MAILGUN_API_KEY']), data=data)
+
+    if response.status_code != 200:
+        logging.error(f'Error sending email: {response.content}')
+
 @app.event_hub_message_trigger(arg_name="azeventhub", event_hub_name="geofeventhub",
                                connection="EventHubConnectionString") 
 def eventhub_trigger(azeventhub: func.EventHubEvent):
     maps_key = os.environ['MAPS_KEY']
     geofence_udid = os.environ['GEOFENCE_UDID']
+    recipient_email = os.environ['RECIPIENT_EMAIL']
 
     event_data = azeventhub.get_body().decode('utf-8')
     event_data = event_data.strip()
@@ -40,8 +58,6 @@ def eventhub_trigger(azeventhub: func.EventHubEvent):
         response = requests.get(url, params=params)
         response_body = json.loads(response.text)
 
-        
-
         distance = response_body['geometries'][0]['distance']
 
         if distance == 999:
@@ -50,5 +66,6 @@ def eventhub_trigger(azeventhub: func.EventHubEvent):
             logging.info(f'Point is just outside geofence by a distance of {distance}m')
         elif distance == -999:
             logging.info(f'Point is inside geofence')
+            send_email('IoT Alert: Point is inside geofence', 'The point is inside the geofence.')
         else:
             logging.info(f'Point is just inside geofence by a distance of {distance}m')
